b2b516867da7ea4a1fd94ba3abbcbdee7f1ce74c
kon <132348293+Kkk0504@users.noreply.github.com>
2023-06-26 11:05:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,8 @@
-// script.js
-
-// ここにJavaScriptコードを追加します
-
-// ホバー時にアイコンを拡大する
-const icons = document.querySelectorAll('.icon');
-
-icons.forEach((icon) => {
-  icon.addEventListener('mouseover', () => {
-    icon.style.transform = 'scale(1.2)';
-  });
-
-  icon.addEventListener('mouseout', () => {
-    icon.style.transform = 'scale(1)';
-  });
-});
-
-// script.js
-
-// フォームの送信前に入力値をチェックする
-const form = document.querySelector('form');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const nameInput = document.getElementById('name');
-  const emailInput = document.getElementById('email');
-
-  if (nameInput.value.trim() === '') {
-    alert('名前を入力してください');
-    return;
+function openImage(element) {
+    var imageSrc = element.getAttribute("src");
+    var downloadLink = document.createElement("a");
+    downloadLink.href = imageSrc;
+    downloadLink.download = "image.jpg";
+    downloadLink.click();
   }
-
-  if (emailInput.value.trim() === '') {
-    alert('メールアドレスを入力してください');
-    return;
-  }
-
-  // フォームの送信処理などを追加する
-  form.submit();
-});
+  
